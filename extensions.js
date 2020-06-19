637ac16c473487b5505ed74c66f85c03a320c3b6
Qucchia <45072410+qucchia@users.noreply.github.com>
2020-06-20 00:26:50 +0200

    Create extensions.js

---
+++ extensions.js
@@ -0,0 +1,13 @@
+class Transform {
+  getInfo() {
+    return {
+      "id": "Transform",
+      "name" "Transform",
+      "blocks": [
+      ],
+      "menus": {
+      }
+    };
+  }
+}
+Scratch.extensions.register(new Transform());
